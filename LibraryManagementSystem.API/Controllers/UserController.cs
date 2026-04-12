52fef71a757c9acd56eaa34c2d2d63abca2b7dc0
Prasad Mahajan <pmmahajan2002@gmail.com>
2026-04-12 20:38:28 +0530

    updated permissions

--- LibraryManagementSystem.API/Controllers/UserController.cs
+++ LibraryManagementSystem.API/Controllers/UserController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LibraryManagementSystem.API.Controllers;
 
 [ApiController]
-[Authorize(Roles = "Admin")]
+[Authorize(Roles = "Admin , User")]
 [Route("api/[controller]/[action]")]
 public class UserController : ControllerBase
 {
