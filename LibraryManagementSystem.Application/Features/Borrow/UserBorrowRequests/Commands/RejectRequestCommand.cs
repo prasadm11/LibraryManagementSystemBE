bcded83f14586173d3eb41c20edd1d40c954aad1
Prasad Mahajan <pmmahajan2002@gmail.com>
2026-04-14 23:03:46 +0530

    added reject response

--- LibraryManagementSystem.Application/Features/Borrow/UserBorrowRequests/Commands/RejectRequestCommand.cs
+++ LibraryManagementSystem.Application/Features/Borrow/UserBorrowRequests/Commands/RejectRequestCommand.cs
@@ -1,5 +1,6 @@
+using LibraryManagementSystem.Application.Features.Borrow.UserBorrowRequests.DTOs;
 using MediatR;
 
 namespace LibraryManagementSystem.Application.Features.Borrow.UserBorrowRequests.Commands;
 
-public record RejectRequestCommand(int id) : IRequest<string>;
+public record RejectRequestCommand(int id) : IRequest<ApproveRequestResponseDto>;
--- LibraryManagementSystem.Application/Features/Borrow/UserBorrowRequests/Handlers/RejectRequestCommandHandler.cs
+++ LibraryManagementSystem.Application/Features/Borrow/UserBorrowRequests/Handlers/RejectRequestCommandHandler.cs
@@ -1,11 +1,12 @@
 using LibraryManagementSystem.Application.Features.Borrow.UserBorrowRequests.Commands;
+using LibraryManagementSystem.Application.Features.Borrow.UserBorrowRequests.DTOs;
 using LibraryManagementSystem.Core.Enums;
 using LibraryManagementSystem.Core.Interfaces.Repositories;
 using MediatR;
 
 namespace LibraryManagementSystem.Application.Features.Borrow.UserBorrowRequests.Handlers;
 
-public class RejectRequestCommandHandler  : IRequestHandler<RejectRequestCommand, string>
+public class RejectRequestCommandHandler  : IRequestHandler<RejectRequestCommand, ApproveRequestResponseDto>
 {
     private readonly IBorrowRequestRepository _borrowRequestRepository;
 
@@ -14,7 +15,7 @@ public class RejectRequestCommandHandler  : IRequestHandler<RejectRequestCommand
         _borrowRequestRepository = borrowRequestRepository;
     }
 
-    public async Task<string> Handle(RejectRequestCommand command, CancellationToken cancellationToken)
+    public async Task<ApproveRequestResponseDto> Handle(RejectRequestCommand command, CancellationToken cancellationToken)
     {
         var request = await _borrowRequestRepository.GetByIdAsync(command.id);
         
@@ -30,7 +31,11 @@ public class RejectRequestCommandHandler  : IRequestHandler<RejectRequestCommand
         request.ApprovedAt = DateTime.UtcNow;
         
         await _borrowRequestRepository.UpdateAsync(request);
-        
-        return "Request rejected successfully";
+
+        var response = new ApproveRequestResponseDto
+        {
+            Message = "Request rejected successfully"
+        };
+        return response;
     }
 }
