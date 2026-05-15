b296824910884b3f29aa8519c797ae44e1ee7197
Prasad Mahajan <pmmahajan2002@gmail.com>
2026-05-15 23:39:25 +0530

    added some changes in createrequest

--- LibraryManagementSystem.Application/Features/Borrow/UserBorrowRequests/Handlers/CreateBorrowRequestHandler.cs
+++ LibraryManagementSystem.Application/Features/Borrow/UserBorrowRequests/Handlers/CreateBorrowRequestHandler.cs
@@ -10,16 +10,54 @@ namespace LibraryManagementSystem.Application.Features.Borrow.UserBorrowRequests
 public class CreateBorrowRequestHandler : IRequestHandler<CreateBorrowRequestCommand , CreateBorrowResponseDto> 
 {
     private readonly IBorrowRequestRepository _borrowRequestRepository;
+    private readonly IBorrowRepository _borrowRepository;
     
-    public CreateBorrowRequestHandler(IBorrowRequestRepository borrowRequestRepository)
+    public CreateBorrowRequestHandler(IBorrowRequestRepository borrowRequestRepository, IBorrowRepository borrowRepository)
     {
         _borrowRequestRepository = borrowRequestRepository;
+        _borrowRepository = borrowRepository;
     }
 
     public async Task<CreateBorrowResponseDto> Handle(CreateBorrowRequestCommand command,
         CancellationToken cancellationToken)
     {
         var request = command.CreateBorrowRequestDto;
+        var borrowRecords = await _borrowRepository.GetUserBorrowRecordsAsync(command.CreateBorrowRequestDto.UserId);
+        var today = DateTime.UtcNow;
+        var activeCount = borrowRecords.Count(x => x.Status == BorrowStatus.Active);
+        var hasOverdue = borrowRecords.Any(x => x.ReturnedAt == null && x.DueDate < today);
+        var hasUnpaidFine = borrowRecords.Any(x => x.FineAmount > 0 && !x.FinePaid);
+        var maxLimitReached = activeCount >= 3;
+        
+        if (hasOverdue)
+
+        {
+
+            throw new Exception(
+
+                "User has overdue books");
+
+        }
+
+        if (hasUnpaidFine)
+
+        {
+
+            throw new Exception(
+
+                "User has unpaid fines");
+
+        }
+
+        if (maxLimitReached)
+
+        {
+
+            throw new Exception(
+
+                "User reached borrow limit");
+
+        }
 
         var createBorrowRequest = new BorrowRecordsUserRequest
         {
