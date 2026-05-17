60c761886c2494835c72fb4c6fccb10403c8e418
Prasad Mahajan <pmmahajan2002@gmail.com>
2026-05-17 11:50:38 +0530

    updated notification feature

--- LibraryManagementSystem.Infrastructure/Repositories/NotificationRepository.cs
+++ LibraryManagementSystem.Infrastructure/Repositories/NotificationRepository.cs
@@ -8,53 +8,47 @@ namespace LibraryManagementSystem.Infrastructure.Repositories;
 public class NotificationRepository : INotificationRepository
 {
     private readonly ApplicationDbContext _dbContext;
+    private readonly IUserRepository _userRepository;
 
-    public NotificationRepository(ApplicationDbContext dbContext)
-
+    public NotificationRepository(ApplicationDbContext dbContext,IUserRepository userRepository)
     {
-
         _dbContext = dbContext;
-
+        _userRepository = userRepository;
     }
 
     public async Task AddAsync(Notification notification)
-
     {
-
         await _dbContext.Notifications.AddAsync(notification);
-
         await _dbContext.SaveChangesAsync();
-
     }
 
     public async Task<List<Notification>> GetByUserIdAsync(int userId)
-
     {
-
+        var user = await _userRepository.GetUserByIdAsync(userId);
+        if (user == null)
+        {
+            throw new KeyNotFoundException($"User with given id {userId} does not exist");
+        }
         var response = await _dbContext.Notifications
-
-            .Where(x => x.UserId == userId)
-
+            .Where(x => x.UserId == userId && !x.IsRead)
             .OrderByDescending(x => x.CreatedAt)
-
             .ToListAsync();
-        
         return response;
-
     }
 
     public async Task MarkAsReadAsync(int userId)
-
     {
-
+        var user = await _userRepository.GetUserByIdAsync(userId);
+        if (user == null)
+        {
+            throw new KeyNotFoundException($"User with given id {userId} does not exist");
+        }
         var notifications = await _dbContext.Notifications.Where(x => x.UserId == userId && !x.IsRead).ToListAsync();
-
         foreach (var notification in notifications)
         {
             notification.IsRead = true;
         }
         
         await _dbContext.SaveChangesAsync();
-
     }
 }
